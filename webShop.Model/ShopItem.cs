29e10cb3de52465a5f0f2650063df3558be66eed
Developer0108 <laurentduquesnoy5@gmail.com>
2022-05-06 11:27:09 +0200

    initial commit

---
+++ webShop.Model/ShopItem.cs
@@ -0,0 +1,10 @@
+namespace webShop.Model;
+
+public class ShopItem
+{
+    public int ItemId {get;set;}
+    public string Name { get; set; }
+    public int Price{get;set;}
+    public string Description {get;set;}
+    
+}
