3dda79e76ad457401dc7a2b1812daa55cd613fc9
Developer0108 <laurentduquesnoy5@gmail.com>
2022-05-31 23:42:12 +0200

    add testing projects

---
+++ webShop.FakeItEasy/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace webShop.FakeItEasy;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ webShop.NSubstitute/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace webShop.NSubstitute;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
